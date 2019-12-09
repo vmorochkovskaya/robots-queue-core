8b1f558a3d52d9005c9f3fc098cf2a90c3f4be1a
vasilisa <vasilisademuanova2700@gmail.com>
2019-12-09 23:50:48 +0300

    add dao class'

--- src/main/java/by/game/core/dao/ILogDao.java
+++ src/main/java/by/game/core/dao/ILogDao.java
@@ -3,7 +3,9 @@ package by.game.core.dao;
 
 import by.game.core.entity.Log;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ILogDao extends CrudRepository<Log, Integer> {
 
    void addLog(Log log);
